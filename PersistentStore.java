88987be088942abcf4152401a177c66eb0cb3741
misomarcell <misomarcell@gmail.com>
2017-02-04 20:58:32 +0100

    Now its more like a dead body than a Skeleton. Don't know thats worse or better.

--- PersistentStore.java
+++ PersistentStore.java
@@ -1,24 +1,20 @@
 import java.util.ArrayList;
 
 public class PersistentStore extends Store{
-	
-	public void storeProduct(Product product)
-	{
-		
-	}
+	public ArrayList<Product> productList = new ArrayList<>();
 	
 	public void createProduct(String type, String name, int priice, int size)
 	{
 		
 	}
 	
-	public ArrayList<Product> loadProducts()
+	/*public ArrayList<Product> loadProducts()
 	{
 		
-	}
+	}*/
 	
 	public void store(Product product)
 	{
-		
+		productList.add(product);
 	}
 }
