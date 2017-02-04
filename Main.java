006029474f3e1f9e300bd2661629f7135cfabec0
misomarcell <misomarcell@gmail.com>
2017-02-04 20:58:46 +0100

    Now its more like a dead body than a Skeleton. Don't know thats worse or better.

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
 public class Main {
 
 	public static void main(String[] args) {
-		StorageManager store = new StorageManager();
+		StorageManager sManager = new StorageManager();
+		StoreCapable store = new StoreCapable();
+		sManager.addStorage(store);
+        sManager.addCDProduct("Hotline Miami OST", 2500, 50);
+        sManager.addBookProduct("Mein Kampf - Harcom", 1090, 500);
+        
+        System.out.println(sManager.listProducts());
+        System.out.println(sManager.getTotalProductPrice());
 
 	}
 
