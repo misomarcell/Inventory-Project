5495ac7b56614e22160e62fb4092585ebc303660
misomarcell <misomarcell@gmail.com>
2017-02-04 20:57:44 +0100

    Now its more like a dead body than a Skeleton. Don't know thats worse or better.

--- StoreCapable.java
+++ StoreCapable.java
@@ -1,21 +1,22 @@
 import java.util.ArrayList;
 
 public class StoreCapable {
-
+	private PersistentStore pStore = new PersistentStore();
 	
 	public ArrayList<Product> getAllProduct()
-	{
-		
-	
+	{	
+		return pStore.productList;
 	}
 	
 	public void storeCDProduct(String name, int price, int track)
 	{
-		
+		Product cdProduct = new CDProduct(name, price, track);
+		pStore.productList.add(cdProduct);
 	}
 	
 	public void storeBookProduct(String name, int price, int size)
 	{
-		
+		Product bookProduct = new BookProduct(name, price, size);
+		pStore.productList.add(bookProduct);
 	}
 }
