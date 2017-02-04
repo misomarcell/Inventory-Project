9cfb793508cbd8021acc72a4c62e32c38ad50013
misomarcell <misomarcell@gmail.com>
2017-02-04 20:58:03 +0100

    Now its more like a dead body than a Skeleton. Don't know thats worse or better.

--- StorageManager.java
+++ StorageManager.java
@@ -1,25 +1,34 @@
+import java.util.ArrayList;
 public class StorageManager {
-
+	private StoreCapable productStorage;
 	
 	public void addStorage(StoreCapable storage)
 	{
-		
+		productStorage = storage;
 	}
 	
 	public void addCDProduct(String name, int price, int track)
 	{
-		
+		productStorage.storeCDProduct(name, price, track);
 	}
 	
 	public void addBookProduct(String name, int price, int size)
 	{
-		
+		productStorage.storeBookProduct(name, price, size);
 	}
 	
 	public String listProducts()
 	{
+		ArrayList<Product> allProducts = new ArrayList<>();
+		allProducts = productStorage.getAllProduct();
 		
-		return "";
+		String stringProducts = "";
+		for (Product product : allProducts)
+		{
+			stringProducts += product.getName() + "\n";
+		}
+		
+		return stringProducts;
 	}
 	
 	public int getTotalProductPrice()
